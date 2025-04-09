206ae106a7e9024635aa5de5eeff847624d6749b
travis-jeans <hello@futurehorizondesign.net.au>
2025-04-09 15:36:19 +1000

    Fixed paths on .gitignore and changed the path for build-themes.sh

--- themes/build-themes.sh
+++ themes/build-themes.sh
@@ -1,4 +1,4 @@
 echo "Building custom themes with Sass."
 sass RBlind-Dark-Loader.scss RBlind-Dark.css && sass RBlind-Light-Loader.scss RBlind-Light.css
 echo "Moving custom themes to /.extra_themes/"
-cp RBlind-Dark.css RBlind-Light.css ../../../../.extra_themes/
+cp RBlind-Dark.css RBlind-Light.css ../../../../../lemmy-ui/.extra_themes/
