2f405860ff2c7cef745bf4a18a2e18551e3f3e0f
travis-jeans <hello@futurehorizondesign.net.au>
2025-05-21 12:47:51 +1000

    Changed the sass argument to sassc so the ruby version of sass is not used.

--- themes/build-themes.sh
+++ themes/build-themes.sh
@@ -1,4 +1,4 @@
 echo "Building custom themes with Sass."
-sass RBlind-Dark.scss RBlind-Dark.css && sass RBlind-Light.scss RBlind-Light.css
+sassc RBlind-Dark.scss RBlind-Dark.css && sassc RBlind-Light.scss RBlind-Light.css
 echo "Moving custom themes to /.extra_themes/"
 cp RBlind-Dark.css RBlind-Light.css ../../../../../lemmy-ui/.extra_themes/
